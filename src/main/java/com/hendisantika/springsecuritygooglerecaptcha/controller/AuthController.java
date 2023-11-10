8258f9e75f0f61af69ab0af41a605de4017f51a0
hendisantika <hendisantika@yahoo.co.id>
2023-11-10 07:57:25 +0700

    #26 - Add token in AuthController ✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/springsecuritygooglerecaptcha/controller/AuthController.java
+++ src/main/java/com/hendisantika/springsecuritygooglerecaptcha/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springsecuritygooglerecaptcha.controller;
 
+import com.hendisantika.springsecuritygooglerecaptcha.model.LoginRequest;
 import com.hendisantika.springsecuritygooglerecaptcha.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +32,12 @@ public class AuthController {
     private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
+
+    @PostMapping("/token")
+    public String token(@RequestBody LoginRequest userLogin) throws AuthenticationException {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userLogin.username(), userLogin.password())
+        );
+        return tokenService.generateToken(authentication);
+    }
 }
