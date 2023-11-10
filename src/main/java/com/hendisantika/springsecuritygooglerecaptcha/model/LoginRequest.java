76bc6cbe8a85e20340bff7fbae5b15c2a285ce5b
hendisantika <hendisantika@yahoo.co.id>
2023-11-10 07:43:14 +0700

    #4 - Add LoginRequest Record ✏️📗:octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/springsecuritygooglerecaptcha/model/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springsecuritygooglerecaptcha.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-google-recaptcha
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/10/23
+ * Time: 07:42
+ * To change this template use File | Settings | File Templates.
+ */
+public record LoginRequest(String username, String password) {
+}
