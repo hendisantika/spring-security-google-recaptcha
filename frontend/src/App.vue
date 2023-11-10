ce46b8bea833135d10053bd65a38ee57f8ac5e36
hendisantika <hendisantika@yahoo.co.id>
2023-11-10 08:11:10 +0700

    #34 - Modify App.vue in frontend project ✏️📗:octocat:🐧🐳

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,58 @@
-<script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import {reactive} from 'vue';
+import {useReCaptcha} from 'vue-recaptcha-v3'
+
+const user = reactive({username: '', password: ''});
+const {executeRecaptcha, recaptchaLoaded} = useReCaptcha()
+
+const onSubmit = async () => {
+  // (optional) Wait until recaptcha has been loaded.
+  await recaptchaLoaded()
+
+  // Execute reCAPTCHA with action "login".
+  const token = await executeRecaptcha('login')
+  console.log("token:", token);
+  console.log("user:", user);
+
+  // Send the token as a header to the login endpoint
+  fetch('http://localhost:8080/api/auth/token', {
+    method: 'POST',
+    headers: {
+      'recaptcha': token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+      .then(r => r.text())
+      .then(data => {
+        console.log("jwt:", data)
+      });
+
+}
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img alt="Vite logo" class="logo" src="/vite.svg"/>
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img alt="Vue logo" class="logo vue" src="./assets/vue.svg"/>
-    </a>
+  <div class="min-h-screen flex bg-gray-200">
+    <form class="w-1/4 m-auto bg-white p-8 border border-gray-400 rounded-md shadow-md" @submit.prevent="onSubmit">
+      <div class="mt-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
+        <input id="username" v-model="user.username" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+               placeholder="Username"
+               type="text">
+      </div>
+      <div class="mt-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+        <input id="password" v-model="user.password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+               placeholder="******************"
+               type="password">
+      </div>
+      <div class="flex justify-end">
+        <button
+            class="g-recaptcha bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Sign In
+        </button>
+      </div>
+    </form>
   </div>
-  <HelloWorld msg="Vite + Vue"/>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
