59de0e044a64d8058e281748ef95bfe19cb460d4
hendisantika <hendisantika@yahoo.co.id>
2023-11-10 07:48:14 +0700

    #11 - Add EnableConfigurationProperties ✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/springsecuritygooglerecaptcha/SpringSecurityGoogleRecaptchaApplication.java
+++ src/main/java/com/hendisantika/springsecuritygooglerecaptcha/SpringSecurityGoogleRecaptchaApplication.java
@@ -2,12 +2,22 @@ package com.hendisantika.springsecuritygooglerecaptcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RecaptchaProperties.class)
 public class SpringSecurityGoogleRecaptchaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityGoogleRecaptchaApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
     }
 
 }
--- src/main/java/com/hendisantika/springsecuritygooglerecaptcha/service/TokenService.java
+++ src/main/java/com/hendisantika/springsecuritygooglerecaptcha/service/TokenService.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springsecuritygooglerecaptcha.service;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
