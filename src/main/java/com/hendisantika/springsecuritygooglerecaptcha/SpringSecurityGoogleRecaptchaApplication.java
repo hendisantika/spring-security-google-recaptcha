2101ccb2d2c2462929e4e2e21e9d209f7a3ad80f
hendisantika <hendisantika@yahoo.co.id>
2023-11-10 07:49:07 +0700

    #12 - Add RecaptchaProperties ✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/springsecuritygooglerecaptcha/SpringSecurityGoogleRecaptchaApplication.java
+++ src/main/java/com/hendisantika/springsecuritygooglerecaptcha/SpringSecurityGoogleRecaptchaApplication.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springsecuritygooglerecaptcha;
 
+import com.hendisantika.springsecuritygooglerecaptcha.security.RecaptchaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
